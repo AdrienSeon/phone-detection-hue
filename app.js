53fa2a208c03b01cf03c4381d06a4aab7d295530
Adrien SEON <1227722+AdrienSeon@users.noreply.github.com>
2021-01-31 23:49:48 +0100

    Changed ping api

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const ping = require ("net-ping");
+const ping = require ("ping");
 const v3 = require('node-hue-api').v3;
 
 const phoneIp = '192.168.1.34';
-const session = ping.createSession();
-let occupancy = "away";
-let firstOccupancy = true;
 const awayRepeatTimer = 10;
 const homeRepeatTimer = 900;
+let firstOccupancy = true;
 
 const hue = v3.api 
-const host = "192.168.1.21"
+const hueBridgeIp = "192.168.1.21"
 const USERNAME = 'rxTnoKgRp7EzTU7vyJqUpR3bg-SqWdcxHZpMp-9Z'
 const GroupLightState = v3.lightStates.GroupLightState;
 const LightState = v3.lightStates.LightState;
@@ -18,45 +16,30 @@ const LightState = v3.lightStates.LightState;
 pingPhone();
 
 function pingPhone() {
-    session.pingHost (phoneIp, function (error, target) {
-        if (error){
-            if (error instanceof ping.RequestTimedOutError){
-                if(process.env.NODE_ENV === 'development') {
-                    console.log (target + ": Not alive");
-                };
-                occupancy = "away";
-                firstOccupancy = true;
-                setTimeout(pingPhone, awayRepeatTimer*1000);
-            }
-            else{
-                if(process.env.NODE_ENV === 'development') {
-                    console.log (target + ": " + error.toString ());
-                };
-                occupancy = "away";
-                firstOccupancy = true;
-                setTimeout(pingPhone, awayRepeatTimer*1000);
-            }
-        } else {
+    ping.sys.probe(phoneIp, function(isAlive, error){
+        if(isAlive){
             if(process.env.NODE_ENV === 'development') {
-                console.log (target + ": Alive");
+                console.log (phoneIp + ": Alive");
             };
-            occupancy = "home";
             if(firstOccupancy) {
                 lightsOn();
                 firstOccupancy = false;
-            }
+            };
             setTimeout(pingPhone, homeRepeatTimer*1000);
-        }
-        if(process.env.NODE_ENV === 'development') {
-            console.log(occupancy);
+        } else {
+            if(process.env.NODE_ENV === 'development') {
+                console.log (phoneIp + ": not reachable");
+            };
+            firstOccupancy = true;
+            setTimeout(pingPhone, awayRepeatTimer*1000);
         };
-    })
+    });
 ;}
 
 
 function lightsOn() {
     // id: 8 = lampe de la télé
-    hue.createLocal(host).connect(USERNAME)
+    hue.createLocal(hueBridgeIp).connect(USERNAME)
         .then(api => {
             const state = new LightState().on()
             return api.lights.setLightState(8, state);
@@ -64,7 +47,7 @@ function lightsOn() {
     ;
 
     // id: 9 = bandeau
-    hue.createLocal(host).connect(USERNAME)
+    hue.createLocal(hueBridgeIp).connect(USERNAME)
         .then(api => {
             const state = new LightState().on()
             return api.lights.setLightState(9, state);
@@ -72,7 +55,7 @@ function lightsOn() {
     ;
 
     // id: 1 = Groupe Salon
-    hue.createLocal(host).connect(USERNAME)
+    hue.createLocal(hueBridgeIp).connect(USERNAME)
         .then(api => {
             const salonState = new GroupLightState()
                 .on()
